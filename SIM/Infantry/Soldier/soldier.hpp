6a2eea78ceff830d07890b0f8a474fe52f7b940d
525jw <525jw@gmail.com>
2025-10-28 21:56:13 +0900

    .

--- SIM/Infantry/Soldier/soldier.hpp
+++ SIM/Infantry/Soldier/soldier.hpp
@@ -49,5 +49,6 @@ public:
         // this->AddCoupling(mnv,"PositionOut",this,"PositionOut",EOC);
         this->AddCoupling(det,"SoldierRep",this,"SoldierRep",EOC);
         this->AddCoupling(fire,"FireOut",this,"FireOut",EOC);
+
     }
 };
